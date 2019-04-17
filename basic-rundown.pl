364cc6a701846b5b2ffeee12a0dfd7c36d9256da
terminalforlife <terminalforlife@yahoo.com>
2019-04-17 16:25:57 +0100

    Updated basic-rundown.pl

--- basic-rundown.pl
+++ basic-rundown.pl
@@ -3,7 +3,7 @@
 #----------------------------------------------------------------------------------
 # Project Name      - perlmisc/basic-rundown.pl
 # Started On        - Wed 17 Apr 11:55:55 BST 2019
-# Last Change       - Wed 17 Apr 14:48:03 BST 2019
+# Last Change       - Wed 17 Apr 16:25:46 BST 2019
 # Author E-Mail     - terminalforlife@yahoo.com
 # Author GitHub     - https://github.com/terminalforlife
 #----------------------------------------------------------------------------------
@@ -33,6 +33,8 @@ printf("\n");
 
 # Setting a variable in Perl. The dollar sign is used to declaring and for calling.
 $A = "true";
+# Alternatively, prefix the above, as below, but it may only work locally.
+my $A = "true";
 
 # If statements in Perl go by 'if(condition){commands};'; they can be one-lined, -
 # as in shell and python. You don't need spacing between {}, unlike in shell. The
@@ -93,3 +95,36 @@ printf("The name I want is at index 0 (first): %s\n", $array[0]);
 
 @string = qw/This is string split by whitespace into an array./;
 print("I want the 2nd index (1) of \$string: $string[1]\n");
+
+# Create a directory in the CWD. As it is, it seems to follow your umask. This is
+# also a basic example of grabbing user input via STDIN.
+print("Create a test directory? ");
+$answer = <STDIN>;
+if($answer =~ m{^[yY]{1}$}){
+	mkdir("Directory_Name");
+	print("Directory created.\n");
+
+	# Now to demonstrate removing a directory, via user prompting.
+	print("Remove created directory? ");
+	$answer = <STDIN>;
+	if($answer =~ m{^[yY]{1}$}){
+		rmdir("Directory_Name");
+		print("Directory removed.\n");
+	}elsif($answer =~ m{^[nN]{1}$}){
+		print("Not removing directory.\n");
+	}else{
+		# Output error message, with extra info, then exit.
+		die "Incorrect response.\n";
+	};
+}elsif($answer =~ m{^[nN]{1}$}){
+	print("Not creating directory.\n");
+}else{
+	# Output error message, with extra info, then exit.
+	die "Incorrect response.\n";
+};
+
+printf("\n");
+
+# Assign a value to $string, then print it with all upper- then lowercase.
+$string = "Test String.";
+printf(qq{Uppercase: "%s"\nLowercase: "%s"\n\n}, uc($string), lc($string));
