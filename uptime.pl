f4993f382534b2f9570b3a869ecf588a4fe71b0c
terminalforlife <terminalforlife@yahoo.com>
2019-04-23 14:49:46 +0100

    Updated uptime.pl; cleaned up

--- uptime.pl
+++ uptime.pl
@@ -3,7 +3,7 @@
 #----------------------------------------------------------------------------------
 # Project Name      - perlmisc/uptime.pl
 # Started On        - Wed 17 Apr 13:13:34 BST 2019
-# Last Change       - Fri 19 Apr 19:21:56 BST 2019
+# Last Change       - Tue 23 Apr 14:49:21 BST 2019
 # Author E-Mail     - terminalforlife@yahoo.com
 # Author GitHub     - https://github.com/terminalforlife
 #----------------------------------------------------------------------------------
@@ -13,37 +13,25 @@
 use strict;
 use warnings;
 
-my $filename = "/proc/uptime";
+if(open(my $FH, '<', '/proc/uptime')){
+	while(<$FH>){
+		my @ARRAY = split(" ", $_);
+		my $HOUR = int($ARRAY[0] / 60 / 60);
+		my $MIN = int($ARRAY[0] / 60 - ($HOUR * 60));
 
-if(-f $filename){
-	open(my $file, '<', $filename);
-
-	while(my $line = <$file>){
-		my @array = split(" ", $line);
-		my $hour = int($array[0] / 60 / 60);
-		my $min = int($array[0] / 60 - ($hour * 60));
-
-		my ($hour_s, $min_s);
-
-		if($hour gt 1 || $hour eq 0){
-			$hour_s = "s";
-		}else{
-			$hour_s = "";
-		};
-
-		if($min gt 1 || $min eq 0){
-			$min_s = "s";
-		}else{
-			$min_s = "";
-		};
+		# Use correct grammar.
+		my $HOUR_S = "s";
+		my $MIN_S = "s";
+		if($HOUR <= 1){$HOUR_S = ""}
+		if($MIN <= 1){$MIN_S = ""}
 
 		printf(
 			"UP: %d hour%s and %d minute%s.\n",
-			$hour, $hour_s, $min, $min_s
-		);
-	};
+			$HOUR, $HOUR_S, $MIN, $MIN_S
+		)
+	}
 
-	close("$file");
+	close($FH)
 }else{
-	die "File '$filename' not found";
+	die "ERROR: $!"
 };
