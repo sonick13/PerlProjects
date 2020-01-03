1614fd9f7fce3c64356f0b2750e380123d4665f7
terminalforlife <terminalforlife@yahoo.com>
2020-01-03 13:07:09 +0000

    Finish POD for `TFL.pm` (for `perldoc`)

--- TFL.pm
+++ TFL.pm
@@ -3,12 +3,14 @@
 #----------------------------------------------------------------------------------
 # Project Name      - PerlProjects/TFL.pm
 # Started On        - Mon  6 May 19:29:05 BST 2019
-# Last Change       - Thu  2 Jan 21:51:40 GMT 2020
+# Last Change       - Fri  3 Jan 13:06:17 GMT 2020
 # Author E-Mail     - terminalforlife@yahoo.com
 # Author GitHub     - https://github.com/terminalforlife
 #----------------------------------------------------------------------------------
 
 package TFL;
+
+=encoding utf8
 
 =head1 NAME
 
@@ -16,7 +18,8 @@ TFL -- key features for TFL programs and general Perl scripts.
 
 =head1 DESCRIPTION
 
-...
+The TFL module provides features written for and used by TFL programs
+and general Perl scripts.
 
 =head1 AUTHORS
 
@@ -28,43 +31,93 @@ require v5.22.1;
 require Exporter;
 
 use strict;
+use warnings;
 use vars '@ISA', '@EXPORT', '$VERSION';
 
 @ISA = 'Exporter';
 
 @EXPORT = (
-	'FErr', 'Err', 'KeyVal', 'DepChk', 'KeyDef', 'UsageCPU', 'UnderLine'
+	'$PROGNAME', '$AUTHOR', '$GITHUB', 'FErr', 'Err', 'KeyVal',
+	'DepChk', 'KeyDef', 'UsageCPU', 'UnderLine'
 );
 
 $VERSION = '2020-01-02';
 
-# Example: FErr(1, __LINE__, "Text for error goes here.")
-# $_[0] = Boolean integer for whether to exit 1 (1) or not (0).
-# $_[1] = Line number; an integer, or, preferably, '__LINE__'.
-# $_[2] = The error message string itself, sans newline character.
+our ($PROGNAME) = $0 =~ m{(?:.*/)?([^/]*)};
+our $AUTHOR = 'written by terminalforlife <terminalforlife@yahoo.com>';
+our $GITHUB = 'https://github.com/terminalforlife';
+
+=head1 VARIABLES
+
+=over 4
+
+=item $TFL::PROGNAME
+
+Contains the name of the current program.
+
+=item $TFL::AUTHOR
+
+Contains the author and E-Mail string for all TFL code.
+
+=item $TFL::GITHUB
+
+Contains the URL for the TFL author's GitHub repositories.
+
+=cut
+
+=back
+
+=head1 FUNCTIONS
+
+=over 4
+
+=item FErr()
+
+Example: FErr(1, __LINE__, "Text for error goes here.")
+$_[0] = Boolean integer for whether to exit 1 (1) or not (0).
+$_[1] = Line number; an integer, or, preferably, '__LINE__'.
+$_[2] = The error message string itself, sans newline character.
+
+=cut
+
 sub FErr{
 	printf("[L%0.4d] ERROR: %s\n", $_[1], $_[2]);
 	exit(1) if $_[0]
 }
 
-# Example: Err(1, "Text for error goes here.")
-# $_[0] = Boolean integer for whether to exit 1 (1) or not (0).
-# $_[1] = The error message string itself, sans newline character.
+=item Err()
+
+Example: Err(1, "Text for error goes here.")
+$_[0] = Boolean integer for whether to exit 1 (1) or not (0).
+$_[1] = The error message string itself, sans newline character.
+
+=cut
+
 sub Err{
 	printf("ERROR: %s\n", $_[1]);
 	exit(1) if $_[0]
 }
 
-# Example: KeyVal($ARGV[0], 0)
-# $_[0] = String 'key=value' to split.
-# $_[1] = Index to return; 0 (key) or 1 (value).
+=item KeyVal()
+
+Example: KeyVal($ARGV[0], 0)
+$_[0] = String 'key=value' to split.
+$_[1] = Index to return; 0 (key) or 1 (value).
+
+=cut
+
 sub KeyVal{
 	return(@{[split('=', $_[0])]}[$_[1]])
 }
 
-# Example: KeyDef(%KEYS)
-# $_[0] = A hash reference of keys whose existence are to be tested.
-# $_[1] = An array reference whose indices contain viable key choices.
+=item KeyDef()
+
+Example: KeyDef(%KEYS)
+$_[0] = A hash reference of keys whose existence are to be tested.
+$_[1] = An array reference whose indices contain viable key choices.
+
+=cut
+
 sub KeyDef{
 	my $Failed = 0;
 	foreach my $KeyViable (@{($_[1])}){
@@ -88,10 +141,15 @@ sub KeyDef{
 	exit(1) if $Failed > 0
 }
 
-# Example_2: DepChk('man')
-# Example_1: DepChk('/usr/bin/man')
-# Example_3: DepChk(':', 'man') || print("Not found.\n")
-# @_ = Executable file for which to search in PATH; basename or absolute path.
+=item DepChk()
+
+Example_2: DepChk('man')
+Example_1: DepChk('/usr/bin/man')
+Example_3: DepChk(':', 'man') || print("Not found.\n")
+@_ = Executable file for which to search in PATH; basename or absolute path.
+
+=cut
+
 sub DepChk{
 	my $Passive = 'false';
 	if ($_[0] eq ':'){
@@ -126,15 +184,25 @@ sub DepChk{
 	}
 }
 
-# Example: UnderLine('-', 'This is an underlined string.')
-# $_[0] = A single character to repeat for each on the line above.
-# $_[1] = The string to underline, such as an important message.
+=item UnderLine()
+
+Example: UnderLine('-', 'This is an underlined string.')
+$_[0] = A single character to repeat for each on the line above.
+$_[1] = The string to underline, such as an important message.
+
+=cut
+
 sub UnderLine{
 	return($_[1] . "\n" . $_[0] x length($_[1]))
 }
 
-# Example: UsageCPU('cpu') | TFL::UsageCPU('cpu2')
-# $_[0] = The CPU to grab the percentage (integer) of its usage. {cpu | cpu[INT]}
+=item UsageCPU()
+
+Example: UsageCPU('cpu') | TFL::UsageCPU('cpu2')
+$_[0] = The CPU to grab the percentage (integer) of its usage. {cpu | cpu[INT]}
+
+=cut
+
 sub UsageCPU{
 	my $StatFile = '/proc/stat';
 
@@ -153,5 +221,45 @@ sub UsageCPU{
 		return(int($Usage))
 	}
 }
+
+=back
+
+=head1 CHANGES
+
+=over 4
+
+=item 2020-01-02
+
+Removed PKGVersion().
+
+Allowed for the exportation of almost all subroutines.
+
+Renamed Defined() to KeyDef(), and FAIL() to FErr().
+
+=item 2019-12-14
+
+Removed i3Do(), DepChkPortable(), _ArgChk(), and UpdChk().
+
+Added Err().
+
+=item 2019-05-16
+
+Added DepChkPortable() and PKGVersion().
+
+=item 2019-05-12
+
+Added i3Do() and UnderLine().
+
+=item 2019-05-07
+
+Added KeyVal(), _ArgChk(), and DepChk().
+
+=item 2019-05-06
+
+TFL module development began.
+
+=back
+
+=cut
 
 return(1)
